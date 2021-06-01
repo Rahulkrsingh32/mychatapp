dc12b67221ff5187764fa63b983502ec4505d73b
Rahul Kumar Singh <rahulkrsingh32@gmail.com>
2021-06-01 23:36:36 +0530

    Updates

---
+++ precache-manifest.1a7b865ef32c154f33f3c44f01f88202.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677a12064fd6eeef583aa83d255bafc8",
+    "url": "/mychatapp/index.html"
+  },
+  {
+    "revision": "2a7bf1d07894528fc605",
+    "url": "/mychatapp/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "acaccc01ddf94e76c3cf",
+    "url": "/mychatapp/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "2a7bf1d07894528fc605",
+    "url": "/mychatapp/static/js/2.33232438.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/mychatapp/static/js/2.33232438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acaccc01ddf94e76c3cf",
+    "url": "/mychatapp/static/js/main.0277d64c.chunk.js"
+  },
+  {
+    "revision": "110af423487a660b4e36",
+    "url": "/mychatapp/static/js/runtime-main.ed09ef35.js"
+  }
+]);
