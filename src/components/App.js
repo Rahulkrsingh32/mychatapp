1d0ccb849706ba04fc3e7dd7522a520f1baae93d
Rahul Kumar Singh <rahulkrsingh32@gmail.com>
2021-06-02 09:59:25 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React from "react"
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-import { AuthProvider } from "../contexts/AuthContext"
+import { AuthProvider } from "../contexts/AuthContext";
 
-import Chats from "./Chats"
-import Login from "./Login"
+import Chats from "./Chats";
+import Login from "./Login";
 
 function App() {
   return (
